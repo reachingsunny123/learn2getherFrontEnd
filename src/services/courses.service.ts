a2d9e340f53f571bdd8d29311dca7a2500a64438
Anirban Mukherjee <reachingsunny123@gmail.com>
2018-03-21 13:06:00 +0530

    added environment specific endpoints

--- src/services/courses.service.ts
+++ src/services/courses.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http'
 import { DataService } from './data.service';
+import { environment } from '../environments/environment';
 
 @Injectable()
 export class CoursesService extends DataService{
   
   constructor(http:Http) {
-    super("http://localhost:8080/courses",http);
+    super(environment.endPoint +"/courses",http);
    }
 
 }
